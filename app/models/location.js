a57f64bf7da34ba28c4fbee58faa4880a72fc228
Joe Karasek <joseph.karasek@gmail.com>
2016-04-12 09:03:14 -0700

    add initial routes and location model

---
+++ app/models/location.js
@@ -0,0 +1,13 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+export default Model.extend({
+  name: attr(),
+  city: attr(),
+  state: attr(),
+  lat: attr(),
+  lon: attr(),
+  // image: attr(),
+  description: attr(),
+  directions: attr()
+});
