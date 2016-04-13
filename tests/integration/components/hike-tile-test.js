2e303fee3be9befee6d1a44218967094f1f623ce
Joe Karasek <joseph.karasek@gmail.com>
2016-04-13 09:28:39 -0700

    integrate data into place tiles, add TODOs

--- tests/integration/components/hike-tile-test.js
+++ tests/integration/components/hike-tile-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('test-weather', 'Integration | Component | test weather', {
+moduleForComponent('hike-tile', 'Integration | Component | hike tile', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{test-weather}}`);
+  this.render(hbs`{{hike-tile}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#test-weather}}
+    {{#hike-tile}}
       template block text
-    {{/test-weather}}
+    {{/hike-tile}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
