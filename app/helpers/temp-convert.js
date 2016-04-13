f3c35a710637fa1afe51534126c91f65802bd4bd
Josh Overly <joshoverly@yahoo.com>
2016-04-13 09:57:13 -0700

    add helper to convert temperature

---
+++ app/helpers/temp-convert.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function tempConvert(params/*, hash*/) {
+  var tempKelvin = params[0];
+  var c = tempKelvin - 273.15;
+  var f = c * 1.8 + 32;
+
+  return f.toFixed(2);
+}
+
+export default Ember.Helper.helper(tempConvert);
---
+++ tests/unit/helpers/temp-convert-test.js
@@ -0,0 +1,10 @@
+import { tempConvert } from 'hike-guide/helpers/temp-convert';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | temp convert');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = tempConvert([42]);
+  assert.ok(result);
+});
