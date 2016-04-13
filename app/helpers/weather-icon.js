c2b55336bfc12799a61a61afb69945bc8fabb752
Josh Overly <joshoverly@yahoo.com>
2016-04-13 13:37:56 -0700

    fix weather icon helper

--- app/helpers/weather-icon.js
+++ app/helpers/weather-icon.js
@@ -5,7 +5,7 @@ export function weatherIcon(params/*, hash*/) {
 
   if (weather === "Rain") {
     return Ember.String.htmlSafe('<span><img src="/assets/img/rain.png" alt="rain-icon"></span>');
-  } else if (weather === "Cloudy") {
+  } else if (weather === "Clouds") {
     return Ember.String.htmlSafe('<span><img src="/assets/img/cloudy.png" alt="cloudy-icon"></span>');
   } else {
     return Ember.String.htmlSafe('<span><img src="/assets/img/sunny.png" alt="sunny-icon"></span>');
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
   model() {
     var trailKey = config.trailApi;
     return $.ajax({
-      url: "https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Phoenix&q[state_cont]=Arizona&radius=50",
+      url: "https://trailapi-trailapi.p.mashape.com/?limit=25&q[activities_activity_type_name_eq]=hiking&q[city_cont]=Portland&q[state_cont]=Oregon&radius=50&rankBy=distance",
       dataType: 'json',
       headers: {
         "X-Mashape-Key": trailKey,
